27519187ef6119fd0c75df1679f967facbb2fc2c
SalavatYapparov <108547219+SalavatYapparov@users.noreply.github.com>
2022-07-18 11:34:16 +0300

    first commit

--- Tasks and exercises/Exercise_2/Program.cs
+++ Tasks and exercises/Exercise_2/Program.cs
@@ -10,13 +10,14 @@ int z2 = Coordinate("z", "B");
 
 int Coordinate(string coorName, string pointName)
 {
-    Console.Write($"Введите координату {coorName} точки {pointName}: ");
-    return Convert.ToInt16(Console.ReadLine());
+Console.Write($"Введите координату {coorName} точки {pointName}: ");
+return Convert.ToInt16(Console.ReadLine());
 }
 
 double Decision(double x1, double x2, 
                 double y1, double y2, 
-                double z1, double z2){
+                double z1, double z2)
+{
   return Math.Sqrt(Math.Pow((x2-x1), 2) + 
                    Math.Pow((y2-y1), 2) + 
                    Math.Pow((z2-z1), 2));
